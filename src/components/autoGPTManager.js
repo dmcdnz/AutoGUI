94a4ac6d453e6abb74e429fcea9cde1887de88b2
dmcdnz <30498409+dmcdnz@users.noreply.github.com>
2023-07-21 13:29:33 +1200

    update

--- src/components/autoGPTManager.js
+++ src/components/autoGPTManager.js
@@ -1,7 +1,9 @@
 const download = require('download-git-repo');
+const fs = require('fs');
+const path = require('path');
 
 function downloadAutoGPT() {
-    download('direct:https://github.com/Significant-Gravitas/Auto-GPT.git', 'path/to/download/to', function (err) {
+    download('direct:https://github.com/Significant-Gravitas/Auto-GPT.git', './auto-gpt', function (err) {
         if (err) {
             console.error(err);
         } else {
@@ -10,6 +12,27 @@ function downloadAutoGPT() {
     });
 }
 
+function checkAndCreateEnvFile() {
+    const envPath = path.join(__dirname, './auto-gpt', '.env');
+    const templatePath = path.join(__dirname, './auto-gpt', '.env.template');
+
+    fs.access(envPath, fs.constants.F_OK, (err) => {
+        if (err) {
+            console.log('.env file does not exist, creating one from .env.template');
+            fs.copyFile(templatePath, envPath, (err) => {
+                if (err) {
+                    console.error('Error creating .env file:', err);
+                } else {
+                    console.log('.env file created successfully');
+                }
+            });
+        } else {
+            console.log('.env file exists');
+        }
+    });
+}
+
 module.exports = {
     downloadAutoGPT,
+    checkAndCreateEnvFile,
 };
